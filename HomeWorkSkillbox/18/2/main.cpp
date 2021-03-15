101450603695b17615310b743a3a6c1de1c21f62
Leonid <leonid8269@gmail.com>
2021-03-15 15:49:15 +0700

    its ok

--- HomeWorkSkillbox/18/2/main.cpp
+++ HomeWorkSkillbox/18/2/main.cpp
@@ -4,16 +4,18 @@
 using namespace std;
 
 int main() {
-    vector<int> array = {2, 11, 15, 7};
+    vector<int> array = { 11, 2, 15, 7};
     int res = 9;
+    bool stop = false; // чтобы не создавать буфферные переменные)
 
     for (int i = 0; i < array.size() ; ++i ){
         for (int j = 0; j < array.size() ; ++j){
             if (array[i] + array[j] == res ) {
                 cout << "First number = " << array[i] << ". Second number = " << array[j] << endl;
+                stop = true;
             }
-            break;
         }
+       if (stop) break; // без нее выводит два раза один и тот же ответ с преставленными меж собой местами xD
     }
 
 
--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 
+using namespace std;
 
 int main() {
+
+    vector<int> vec = {7, 1, 5, 4, 6, -20, 100, 5, 3};
+
+
+
+
+
 
     return 0;
 }
