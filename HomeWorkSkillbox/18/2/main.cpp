88820ff6f4b0ac22149bc64191f7c7a79ceae8d8
Leonid <leonid8269@gmail.com>
2021-03-16 09:22:26 +0700

    build 1.0

--- HomeWorkSkillbox/18/2/main.cpp
+++ HomeWorkSkillbox/18/2/main.cpp
@@ -4,20 +4,20 @@
 using namespace std;
 
 int main() {
-    vector<int> array = { 11, 2, 15, 7};
-    int res = 9, buf = 0, buf1 = 0 ;
-
+    vector<int> array = {2, 11, 15, 7};
+    int res = 9;
+    bool stop = false; // чтобы не создавать буфферные переменные)
 
     for (int i = 0; i < array.size() ; ++i ){
         for (int j = 0; j < array.size() ; ++j){
             if (array[i] + array[j] == res ) {
-                 buf = array[i];
-                 buf1 = array[j];
+                cout << "First number = " << array[i] << ". Second number = " << array[j] << endl;
+                stop = true;
             }
+            break;
         }
-       //if (stop) break; // без нее выводит два раза один и тот же ответ с преставленными меж собой местами xD
+        if (stop) break; // без нее выводит два раза один и тот же ответ с преставленными меж собой местами xD
     }
-    cout << buf << " " << buf1;
 
     return 0;
 }
