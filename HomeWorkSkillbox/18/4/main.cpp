230d622569cab37a4447e462b62f1f9d50efa397
Leonid <leonid8269@gmail.com>
2021-03-16 18:22:00 +0700

    OK!

--- HomeWorkSkillbox/18/4/main.cpp
+++ HomeWorkSkillbox/18/4/main.cpp
@@ -6,7 +6,16 @@ using namespace std;
 int main() {
 
     vector<int> vec = {-100,-50, -5, 1, 10, 15};
+    for (int i = 0 ; i < vec.size() ; ++i) {
+        for (int j = 0; j < vec.size() - i - 1; ++j) {
 
-
+            if (abs(vec[j]) > abs(vec[j + 1])) {
+                int buf = vec[j];
+                vec[j] = vec[j + 1];
+                vec[j + 1] = buf;
+            }
+        }
+    }
+    for (int i = 0;i < vec.size() ; ++i) cout << vec[i] << " ";
     return 0;
 }
