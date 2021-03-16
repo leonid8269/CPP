46f68afe9c6cd4f5fa03eff8022fea0ceae1f325
Leonid <leonid8269@gmail.com>
2021-03-16 16:34:30 +0700

    go push

--- HomeWorkSkillbox/18/1/main.cpp
+++ HomeWorkSkillbox/18/1/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    vector<int> arr = {-2,1,-3,4,-1,2,1,-5,4}; // нахожджение максимального значения из нескольких
+    int buf = 0, max = 0;
+    for(int i = 0; i < arr.size() ; ++i){
+        for (int j = i; j < arr.size() ;++j){
+            buf += arr[j];
+            if (buf > max) {
+                max = buf;
+            }
+        }
+        buf = 0;
+    }
+    cout << max <<" "<< buf ;
+
     return 0;
 }
--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -9,9 +9,17 @@ int main() {
 
     vector<int> vec = {7, 1, 5, 4, 6, -20, 100, 5, 3};
 
+    for (int i = 0 ; i < vec.size() ; ++i){
+        for (int j = 0; j < vec.size() - i -1; ++j){
 
-
-
+            if(vec[j] > vec[j+1]){
+                int temp = vec[j];
+                vec[j] = vec[j+1];
+                vec[j+1] = temp;
+            }
+        }
+    }
+    for (int i = 0 ;i < vec.size();++i) cout << vec[i]<< " ";
 
 
     return 0;
