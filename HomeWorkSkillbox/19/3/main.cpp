3b4a4739282285ab15466294e3c687c1ca8df188
Leonid <leonid8269@gmail.com>
2021-03-21 17:00:45 +0700

    checkpoint 17:00

--- HomeWorkSkillbox/19/3/main.cpp
+++ HomeWorkSkillbox/19/3/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+using namespace std;
+
+bool chekString (const string& num, int& half){
+    int matches = 0;
+    for (int i = 0; i < num.length(); ++i){
+        if (num[i] == '/' || num[i] == '*' || num[i] == '-' || num[i] == '+'){
+            ++ matches;
+            half = i;
+        }
+        if (num[i] < '0' || num[i] > '9' ) return false;
+    }
+    if (matches == 0 || matches > 1) return false;
+    return true;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    string calculator;
+    int action = 0; // действие осуществляемое над числами
+
+    do {
+        cin >> calculator;
+    }while (!chekString(calculator, action));
+
+
+
     return 0;
 }
