afb056d6a065aa48599a670ef6fbf77723fee2f3
Elizov_leonid <leonid8269@gmail.com>
2021-03-22 21:22:29 +0700

    Began to write a function: calculationProcess!!! End day. Goodbye

--- HomeWorkSkillbox/19/3/main.cpp
+++ HomeWorkSkillbox/19/3/main.cpp
@@ -1,18 +1,46 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-bool chekString (const string& num, int& half){
+
+bool checkPoint(const string& num, int beginning, int border){
+
+    int point = 0;
+    for (int i = beginning; i < border; ++i){
+        if (num[i] == '.') ++point;
+        if (point > 1) return false;
+    }
+    return true;
+}
+
+bool chekString (const string& num, int rowSeparator){
     int matches = 0;
     for (int i = 0; i < num.length(); ++i){
+        if (num[i] < '*' || num[i] == ',' || num[i] > '9' ) return false;       //! таблица ASCII впомощь ;)
+
         if (num[i] == '/' || num[i] == '*' || num[i] == '-' || num[i] == '+'){
             ++ matches;
-            half = i;
+            rowSeparator = i;
         }
-        if (num[i] < '0' || num[i] > '9' ) return false;
     }
+
     if (matches == 0 || matches > 1) return false;
+
+    if (!checkPoint(num, 0, rowSeparator )) return false;                 //! Где 2 аргумент - с какого места начнется проверка. 3 аргумент - до какого
+    if (!checkPoint(num, rowSeparator + 1, num.length())) return false;   //! + 1 - начало проверки после разделителя
+
+
     return true;
+}
+
+void calculationProcess (const string& str, int half){
+
+    double first;
+    double second;
+
+
+
 }
 
 
@@ -24,6 +52,7 @@ int main() {
         cin >> calculator;
     }while (!chekString(calculator, action));
 
+    calculationProcess(calculator, action);
 
 
     return 0;
