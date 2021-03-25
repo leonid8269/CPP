6868f952858b32464b212f6be0d9eb84b6c0dcc9
Leonid <leonid8269@gmail.com>
2021-03-25 17:50:04 +0700

    save for check buf int or string

--- HomeWorkSkillbox/19/4/main.cpp
+++ HomeWorkSkillbox/19/4/main.cpp
@@ -3,20 +3,56 @@
 using namespace std;
 
 enum notes{
-    C = 64, //!  1000000    C - нота до
-    D = 32, //!  0100000    D - ре
-    E = 16, //!  0010000    E - ми
-    F = 8,  //!  0001000    F - фа
-    G = 4,  //!  0000100    G - соль
-    A = 2,  //!  0000010    A - ля
-    H = 1   //!  0000001    H или B - си
+    C,    //! C - нота до
+    D,        //!      D - ре
+    E,        //!      E - ми
+    F,        //!     F - фа
+    G,        //!     G - соль
+    A,  //!     A - ля
+    H   //!     H или B - си
 };
+bool checkKey(const string& buf){
+    for (int i = 0 ; i < buf.length() ; ++i)
+        if (buf[i]  < 1 || buf[i] > 7 ) return false;
+    return true;
+}
 
+void enterMusic(string& track){
+    string buf;
 
+    for (int i = 0; i < 12; ++i){
+        cin >> buf;
+        if (!checkKey(buf)){
+            --i;
+            buf = "";
+            cout << "The last combination you entered is not correct. Try again";
+        }
+        track += buf + ' ';
+    }
+
+}
+
+void viewMusic (const string& music){
+    int one, two, free, four, five, six, seven;
+
+    one = 1 << C ;
+    two = 1 << D ;
+    free = 1 << E;
+    four = 1 << F;
+    five = 1 << G;
+    six = 1 << A;
+    seven = 1 << H;
+
+    cout << one << " " << two << " " << free << " " << four << " " <<
+    five << " " << six << " " << seven;
+
+}
 
 
 int main() {
-
+    string track;
+    //enterMusic(track);
+    viewMusic(track);
 
     return 0;
 }
