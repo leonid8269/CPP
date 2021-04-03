5a39533cd8f5d7ce5f0a677d0672b01deae16738
Leonid <leonid8269@gmail.com>
2021-04-03 18:09:12 +0700

    add

--- LearningProcess/20/Pointers to strings/main.cpp
+++ LearningProcess/20/Pointers to strings/main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+
+using namespace std;
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    char s[5] = {'h', 'e', 'l', 'l', 'o'};
+
+    const char* a = "Hello world!";
+    const char b [] = "Hello world!";
+
+    int q[] = {1,2,3};
+    cout << q << endl; // выведет адрес
+    cout << b << endl; // выведет всю строку,
+    cout << &b << endl; // с амперсантом выдает адрес
+
+    char x[] = "Hello World";
+    for(int i = 0; *(x+i) != '\0' ;++i)cout << *(x+i);
+
     return 0;
 }
