88487ce816ab329a54f1928164cb581e70c38d53
Leonid <leonid8269@gmail.com>
2021-04-03 15:31:35 +0700

    sent it for review

--- HomeWorkSkillbox/19/5/main.cpp
+++ HomeWorkSkillbox/19/5/main.cpp
@@ -10,8 +10,8 @@ enum shield{
     POWER_HOUSE = 1,        //!ВКЛ/ВЫКЛ эл. энергия
     SOCKETS = 2,            //!Розетки
     LIGHT_HOUSE = 4,        //!Свет в доме
-    LIGHT_STREET = 8,       //!Свет улица
-    HEAT_HOUSE = 16,         //!Свет дома
+    LIGHT_GARDEN = 8,       //!Свет сад
+    HEAT_HOUSE = 16,         //!Отопление дома
     HEAT_WATER_WELL = 32,    //!отопление трубопровода
     AIR_COOL = 64            //!кондиционер
 };
@@ -31,29 +31,93 @@ void timeInitialization(string &day, string &month, string &numMonth, int &hour,
     time.clear();   //сброс флагов ошибок (нужно почитать что это)
 }
 
+
+void accomplishment(double temperatureOutside,double temperatureInside,
+                     string& movingOutside, string& powerLight,
+                     int hour,  int& statusSystem, int& tempLight){ // выполнение основы программы...
+
+    if (temperatureOutside < 0 && !(statusSystem & HEAT_WATER_WELL)) {
+        statusSystem |= HEAT_WATER_WELL;
+        cout << "The water pipe heating system is turned ON!\n";
+    }else if (temperatureOutside >= 5 && (statusSystem & HEAT_WATER_WELL)){
+        statusSystem &= ~ HEAT_WATER_WELL;
+        cout << "The water supply heating system is turned OFF\n";
+    }
+
+    if (((hour >= 16 || hour <= 5) && movingOutside == "yes") && !(statusSystem & LIGHT_GARDEN)) {
+        statusSystem |= LIGHT_GARDEN;
+        cout << "Turning ON garden lighting\n";
+    }else if (((hour < 16 && hour >= 5) && movingOutside == "no") && (statusSystem & LIGHT_GARDEN)) {
+        statusSystem &= ~ LIGHT_GARDEN;
+        cout << "Turning OFF garden lighting\n";
+    }
+
+    if ((temperatureInside < 22) && !(statusSystem & HEAT_HOUSE)){
+        statusSystem |= HEAT_HOUSE;
+        cout << "Heating in the house is ON\n";
+    }else if (temperatureInside > 25 && (statusSystem & HEAT_HOUSE)){
+        statusSystem &= ~ HEAT_HOUSE;
+        cout << "Heating in the house is OFF\n";
+    }
+
+    if ((temperatureInside >= 30) && !(statusSystem & AIR_COOL)){
+        statusSystem |= AIR_COOL;
+        cout << "Air cool ON\n";
+    }else if((temperatureInside <= 25) && (statusSystem & AIR_COOL)){
+        statusSystem &= ~ AIR_COOL;
+        cout << "Air cool OFF\n";
+    }
+
+    if (powerLight == "on"){
+        statusSystem |= LIGHT_HOUSE;
+    }else {
+        statusSystem &= ~LIGHT_HOUSE;
+    }
+
+    if ((hour >= 16 && hour < 20) && (statusSystem & LIGHT_HOUSE)){
+        tempLight = 2700;
+    }else if ((hour < 16 || hour >= 20) && (statusSystem & LIGHT_HOUSE)) {
+        tempLight = 5000;
+    }
+    if (statusSystem & LIGHT_HOUSE) cout << "Current color temperature " << tempLight << "K\n";
+
+
+}
+
+bool chekCurrentData(const string& movingOutside, const string& powerLight){
+    return (movingOutside == "yes" || movingOutside == "no" && powerLight == "on" || powerLight == "off");
+}
+
 void dataCollection (int hour, int minute, double temperatureOutside,
                      double temperatureInside,string& movingOutside,
-                     string& powerLight){ // в данной функции будет проводится сбор информации с датчиков.
+                     string& powerLight, int& statusSystem, int& tempLight){ // в данной функции будет проводится сбор информации с датчиков.
 
     for (int day = 0; day < 2  ;++day){
         for (int step = 0;step < 24 ; ++step, ++hour){
             if (hour == 24) hour = 0;
             string zero;
-            if (hour < 10) zero = "0";
-            cout << "Status on  " << zero <<hour << ":" << minute << ".\n";
+            if (minute < 10) zero = "0";
+            cout << "Status on  "  << hour << ":" << zero << minute << ".\n";
+            cout << "Enter <temperature outside> <temperature inside> <yes and no moving outside?> <off and on light?>\n";
 
+            do {
 
+                cin >> temperatureOutside >> temperatureInside >> movingOutside >> powerLight;
+               if (chekCurrentData(movingOutside, powerLight)) break;
+               else {
+                   cout << "\aError, Please try again\n";
+               }
+            }while(true);
+            accomplishment(temperatureOutside, temperatureInside, movingOutside,powerLight,
+                           hour, statusSystem,  tempLight);
         }
     }
-
-
 
 }
 
 int main() {
     string day, month, numMonth;
     int hour, minute;
-
 
     timeInitialization(day, month, numMonth, hour, minute);
 
@@ -62,7 +126,7 @@ int main() {
     if (minute < 10) zero = "0"; // чтобы небыло 20:6, а было 20:06
 
     cout << "Hello, today is " <<  day << " the " << numMonth << "th. \n"
-            "Now is the time: " << hour << ":"  << zero << minute << ".\n"
+            "Now is the time: "  << hour << ":"  << zero << minute << ".\n"
             "I am beta-version home caregiver!\n"
             "I will keep an eye on the climate and the comfort of the house. \n"
             "Have a nice weekend break\n"
@@ -78,17 +142,23 @@ int main() {
 
     Sleep(3000);
     statusSystem |= POWER_HOUSE;
+    statusSystem |= SOCKETS; // розетки всегдла вкл, когда включается рубильник.
+
     cout << "\nThank you for waiting...\n";
     cout << "Have a good two days!!\n";
 
     double temperatureOutside = 0, temperatureInside = 0;
     string movingOutside, powerLight;
+    int tempLight = 0;
 
     dataCollection (hour, minute, temperatureOutside, temperatureInside,
-                    movingOutside, powerLight);
+                    movingOutside, powerLight, statusSystem, tempLight);
 
-
-
-
+    cout << "I hope your weekend was great!\n"
+            "Time to say goodbye\n"
+            "Come again\n"
+            "Shutting down the work...";
+    Sleep(3000);
+    statusSystem = 0;
     return 0;
 }
