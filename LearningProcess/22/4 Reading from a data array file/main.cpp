1e1db3b85589e4292f40e68df05303ab304f1b4b
Leonid <leonid8269@gmail.com>
2021-04-10 15:34:09 +0700

    go check 1 task

--- LearningProcess/22/4 Reading from a data array file/main.cpp
+++ LearningProcess/22/4 Reading from a data array file/main.cpp
@@ -1,16 +1,19 @@
 #include <iostream>
 #include <fstream>
-
+#include <vector>
 
 int main() {
     using namespace std;
-    double purse[4];
+    double purse[100];
 
     ifstream bank;
     bank.open("C:\\CPP\\LearningProcess\\22\\4 Reading from a data array file\\bank.txt");
-    for (int i = 0; i < 4  ; ++i)bank >> purse[i];
-    for (int i = 0; i < 4; ++i ) cout << purse[i] << " ";
-
+    int count = 0;
+    while (!bank.eof()){
+        bank >> purse[count];
+        count += 1;
+    }
+    for (int i = 0; i < count; ++i ) cout << purse[i] << " ";
 
     bank.close();
 
