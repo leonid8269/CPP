649f2640423698c528029c6d82fc846c7405cb43
Leonid <leonid8269@gmail.com>
2021-04-17 10:48:13 +0700

    ho cheking

--- HomeWorkSkillbox/22/2/main.cpp
+++ HomeWorkSkillbox/22/2/main.cpp
@@ -10,13 +10,15 @@ int main() {
 
     string path = "C:\\CPP\\HomeWorkSkillbox\\22\\2\\text.txt";
     ifstream text;
-    text.open(path, ifstream::binary);
+    text.open(path);
     if (!text.is_open()) cout << "Error opening file \n\a";
     else {
         while (!text.eof()){
-           char works [20];
-           text.read(works, sizeof(works));
-           for (int i = 0; i < sizeof(works) ;++i) cout << works[i];
+           char works [20] ;
+           text.read(works, sizeof(works) - 1); // -1 для того чтобы записывалось до \0
+          // for (int i = 0; i < sizeof(works) ;++i)
+          works[19] = 0;
+               cout << works/*[i]*/;
         }
     }
     text.close();
