950c6874e9c3be9d4f72e82dd7fea01b7242d28c
Leonid <leonid8269@gmail.com>
2021-04-19 09:21:30 +0700

    add gcount

--- HomeWorkSkillbox/22/2/main.cpp
+++ HomeWorkSkillbox/22/2/main.cpp
@@ -14,10 +14,10 @@ int main() {
     if (!text.is_open()) cout << "Error opening file \n\a";
     else {
         while (!text.eof()){
-           char works [20] ;
-            works[19] = 0;
-           text.read(works, sizeof(works) - 1); // -1 для того чтобы записывалось до \0
-               cout << works;
+           char works [20];
+           text.read(works, sizeof(works) - 1);
+           works[text.gcount()] = 0;
+           cout << works;
         }
     }
     text.close();
