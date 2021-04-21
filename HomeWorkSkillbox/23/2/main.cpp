949c7cc18788a479cc3198e5b12db3541a6e7a55
Leonid <leonid8269@gmail.com>
2021-04-21 10:00:57 +0700

    1

---
+++ HomeWorkSkillbox/23/2/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <cstring>
+
+int main() {
+    using namespace std;
+    srand(time(nullptr));
+
+    ofstream pictures ("..\\pic.txt");
+
+    int width = 0;
+    cout << "Enter the width:\n";
+    cin >> width;
+
+    cout << "Enter the height:\n";
+    int height = 0;
+    cin >> height;
+
+    for (int i = 0;i < width ;++i){
+        for (int j = 0; j < height ;++j)pictures << rand() % 2;
+        pictures << endl;
+    }
+
+    pictures.close();
+    return 0;
+}
