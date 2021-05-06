df56b237ef1e04469ba0f960c5a665aedfcb1cdb
Leonid <leonid8269@gmail.com>
2021-05-06 18:44:32 +0700

    1

--- HomeWorkSkillbox/25/2/main.cpp
+++ HomeWorkSkillbox/25/2/main.cpp
@@ -2,10 +2,9 @@
 #include <map>
 #include <string>
 
-
 int main() {
 
-    std::map<int,std::string> regist;
+    std::map<std::string,std::string> regist;
 
     int count = 0;
     int i = 0;
@@ -14,19 +13,15 @@ int main() {
         std::cout << "Enter next or lastname the patient\n";
         std::cin >> ans;
         if (ans == "next") {
-            for (std::map<int,std::string>::iterator it = regist.begin();
-                    it != regist.end(); it ++){
-
-            }
+            std::cout <<regist.begin()->second << std::endl;
+            regist.erase(regist.begin());
         }
         else {
-            ++count;
-            regist.insert(std::make_pair(i, ans));
+            count+= 2;
+            regist.insert(std::make_pair(ans + std::to_string(i), ans));
         }
+        i++;
     }while (i < count);
-
-
-
 
     return 0;
 }
--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -11,17 +11,16 @@
 
 int main() {
 
-    std::map<int, std::string> maps;
-    maps.insert(std::pair<int,std::string>(1, "f"));
-    maps.insert(std::pair<int,std::string>(2, "b"));
-    maps.insert(std::pair<int,std::string>(3, "d"));
-    maps.insert(std::pair<int,std::string>(4, "a"));
-    maps.insert(std::pair<int,std::string>(5, "e"));
-    maps.insert(std::pair<int,std::string>(6, "c"));
+    std::map<std::string, std::string> maps;
+    for (int i = 0; i < 5 ;++i){
+        std::string name;
+        std::cin >> name;
+        maps.insert(std::make_pair(name + std::to_string(i), name ));
+    }
 
-    for (std::map<int, std::string>::iterator it = maps.begin();
+    for (auto it = maps.begin();
             it!=maps.end();++it){
-
+        std::cout << it->second << std::endl;
     }
 
 
