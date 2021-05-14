a7c0fe3da5fcc187b5cd6769fded5b9d3ee90951
Leonid <leonid8269@gmail.com>
2021-05-14 13:24:21 +0700

    kak zadumivalos

--- HomeWorkSkillbox/26/3/main.cpp
+++ HomeWorkSkillbox/26/3/main.cpp
@@ -1,17 +1,45 @@
 #include <iostream>
 
 #define SPRING 1
-#define SUMMER 1
-#define AUTUMN 1
-#define WINTER 1
-
-#define NAME(x) #x
-
+#define SUMMER 0
+#define AUTUMN 0
+#define WINTER 0
 
 #if ( SPRING ) // в скобки введи макрос сезона который хочешь увидить
 int main(){
 
-    std::cout << NAME(SPRING); // в скобки введи макрос сезона который хочешь увидить
+    std::cout << "Spring"; // в скобки введи макрос сезона который хочешь увидить
+
+    return 0;
+}
+
+#endif
+
+#if ( SUMMER ) // в скобки введи макрос сезона который хочешь увидить
+int main(){
+
+    std::cout << "Summer"; // в скобки введи макрос сезона который хочешь увидить
+
+    return 0;
+}
+
+#endif
+
+#if ( AUTUMN ) // в скобки введи макрос сезона который хочешь увидить
+int main(){
+
+    std::cout << "Autumn"; // в скобки введи макрос сезона который хочешь увидить
+
+    return 0;
+}
+
+#endif
+
+
+#if ( WINTER ) // в скобки введи макрос сезона который хочешь увидить
+int main(){
+
+    std::cout << "Winter"; // в скобки введи макрос сезона который хочешь увидить
 
     return 0;
 }
--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -3,12 +3,23 @@
 #include <map>
 
 int main(){
-    std::map<std::string,int> reg;
-    reg.insert(std::make_pair("bvanov", 1));
-    reg.insert(std::make_pair("aaetrov", 1));
-    auto nameseek = reg.find("bvanov");
-    if (nameseek->first == "bvsdanov"){
-        std::cout << "seeking\n";
+    int32_t length{};
+    int32_t sign{};
+    while (true) {
+        length = -1;
+        sign = -1;
+
+        std::cout << "length: \n";
+        while (length < 0) std::cin >> length;
+        std::cout << "sign: \n";
+        while (sign < 0) std::cin >> sign;
+        if (length + sign < 40) break;
+    }
+
+    for (int i = 0; i < length + sign; ++i){
+        if (i < length / 2) std::cout << '!';
+        else if (i < (length / 2) + sign) std::cout << '~';
+        else std::cout << '!';
     }
 
 
