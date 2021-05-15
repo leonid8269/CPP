9d041c9f19aee51b9e5ffa89c0701245ef0c9e5f
Leonid <leonid8269@gmail.com>
2021-05-15 18:33:55 +0700

    first commit

---
+++ HomeWorkSkillbox/27_time/1/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+#include "fstream"
+#include <sstream>
+
+enum {
+    START,
+    STOP
+};
+
+struct s_task {
+    time_t time[2]{};
+    double time_execute{};
+
+    uint8_t hour[2]{}, min[2]{};
+    std::string name;
+    bool flag_process{}; // если флаг тру значит задача выполняется
+};
+
+
+void time_label (s_task &task, uint8_t value){
+    task.time[value] = std::time(nullptr);
+    std::tm* local = std::localtime(&task.time[value]);
+
+    task.hour[value] = local->tm_hour;
+    task.min[value] = local->tm_min;
+
+}
+
+     //! начало задачи, то есть определяется время начала и название
+void start_task(s_task& task){
+    std::cout << "Enter the name of the task: \n";
+
+    std::cin.ignore();
+    getline(std::cin, task.name);
+
+
+    time_label(task, START);
+    task.flag_process = true;
+
+    std::ofstream save("..\\save.txt", std::ios::app);
+    save << "Start time [" << std::to_string (task.hour[START]) << ':' << std::to_string (task.min[START]) <<
+            "] name a task \"" << task.name << "\" ";
+    save.close();
+
+
+}
+
+void end_task(s_task& task){
+    time_label(task, STOP);
+
+    task.flag_process = false; // закончился прогресс этой задачи
+    task.time_execute = difftime(task.time[STOP], task.time[START]);
+
+    std::ofstream save("..\\save.txt", std::ios::app);
+    save << "Stop time ["<< std::to_string (task.hour[STOP]) << ':' << std::to_string (task.min[STOP]) << "] " <<
+    "Executed per second = " << task.time_execute << " sec."<< std::endl;
+    save.close();
+}
+
+
+void status(){
+    std::ifstream load ("..\\save.txt");
+    if(!load.is_open()){
+        std::cout << "Not found file\a \"save.bin\" !!!\n";
+    }else {
+        while (!load.eof()){
+            std::string str;
+            getline(load, str);
+            std::cout << str << '\n';
+        }
+    }
+    load.close();
+}
+
+int main() {
+
+    s_task task;
+    while (true){
+        std::string answer{};
+        std::cout << "Enter command:\n";
+        std::cin >> answer;
+
+
+        if (answer == "begin"){
+            if (task.flag_process){
+                end_task(task);
+                start_task(task);
+            }else{
+                start_task(task);
+            }
+
+        }else if (answer == "end"){
+            if (task.flag_process) end_task(task);
+            else std::cout << "No active tasks found!\n\a";
+
+        }else if (answer == "status"){
+            status();
+
+        }else if (answer == "exit"){
+            std::cout << "Goodbye!\n";
+            break;
+
+        }else {
+            std::cout << "This command not found!\n"
+                         "Try again\n";
+        }
+    }
+    return 0;
+}
--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
 #include <cstdint>
 #include <map>
+#include <ctime>
+#include <iomanip>
+#include <fstream>
 
-int main(){
-    int32_t length{};
-    int32_t sign{};
-    while (true) {
-        length = -1;
-        sign = -1;
 
-        std::cout << "length: \n";
-        while (length < 0) std::cin >> length;
-        std::cout << "sign: \n";
-        while (sign < 0) std::cin >> sign;
-        if (length + sign < 40) break;
-    }
 
-    for (int i = 0; i < length + sign; ++i){
-        if (i < length / 2) std::cout << '!';
-        else if (i < (length / 2) + sign) std::cout << '~';
-        else std::cout << '!';
-    }
-
+int main (){
 
     return 0;
 }
