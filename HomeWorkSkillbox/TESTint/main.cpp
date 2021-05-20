694654f1e26f4b4024466d34378aa870d8c16324
Leonid <leonid8269@gmail.com>
2021-05-20 17:14:28 +0700

    bad

--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -13,13 +13,11 @@ int main (){
     time_t t = time(nullptr);
     time_t s = time(nullptr);
 
-    std::tm today = *std::localtime(&s);
-    std::tm t_tm = *std::localtime(&t);
+    std::tm today = *std::localtime(&s);std::tm t_tm = *std::localtime(&t);
 
     std::cin >> std::get_time(&t_tm, "%Y/%m/%d");
 
 
-    std::cout << "to = " << today.tm_yday << " be = " << t_tm.tm_yday;
 
 
     return 0;
