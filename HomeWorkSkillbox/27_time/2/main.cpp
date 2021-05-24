6f03e2953712e81c38e8ed0d5a68fc8486b95c86
Leonid <leonid8269@gmail.com>
2021-05-24 15:31:05 +0700

    go

--- HomeWorkSkillbox/27_time/2/main.cpp
+++ HomeWorkSkillbox/27_time/2/main.cpp
@@ -5,17 +5,16 @@
 #include <string>
 #include <vector>
 
+#define MDAY (friends[i].data.tm_mon * 30) + friends[i].data.tm_mday
+#define CHECK_1 (friends[i].data.tm_mon == today.tm_mon && friends[i].data.tm_mday > today.tm_mday) || \
+                    friends[i].data.tm_mon > today.tm_mon
 
-enum {
-    YEAR,
-    MONTH,
-    DAY,
-    DAY_IN_YEAR
-};
+#define TODAY_BIRTHDAY friends[i].data.tm_mon == today.tm_mon && friends[i].data.tm_mday == today.tm_mday
+#define YEARS   today.tm_year - friends[i].data.tm_year
 
 struct birthday{
     std::string name;
-    uint8_t date[4]{};
+    tm data{};
 };
 
 
@@ -35,45 +34,31 @@ int main() {
             std::cout << "Enter the driving date in YYYY/MM/DD format\n";
 
             time_t t = time(nullptr);
-            std::tm tm_t = *std::localtime(&t);
-            std::cin >> std::get_time(&tm_t, "%Y/%m/%d");
-
-            friends[priority].date[YEAR]  = tm_t.tm_year;
-            friends[priority].date[MONTH] = tm_t.tm_mon;
-            friends[priority].date[DAY]   = tm_t.tm_mday;
-            friends[priority].date[DAY_IN_YEAR] = tm_t.tm_mon * 30 + tm_t.tm_mday;
-
+            friends[priority].data = *std::localtime(&t);
+            std::cin >> std::get_time(&friends[priority].data, "%Y/%m/%d");
             ++priority;
         }
     }while (true);
 
-
-    //! время сейчас
     time_t t = time(nullptr);
-    std::tm tm_t = *std::localtime(&t);
-
-
- //! Есть проблема,  нужна сортировка пузырьком по дням-месяцам рождениям! или другой вариант)
-    for (int i = 0; i < friends.size(); ++i ) {
-        if (tm_t.tm_mon == friends[i].date[MONTH] && tm_t.tm_mday == friends[i].date[DAY]) {
-            std::cout << "Today is the birthday of " << friends[i].name << ".\nAnd it is executed "
-                      << tm_t.tm_year - friends[i].date[YEAR] << " year\n\n";
-        }
-    }
+    std::tm today = *std::localtime(&t);
 
     uint8_t minDay = UINT8_MAX;
-
-    for (int j = 0; j < friends.size(); ++j) {
-        if (friends[j].date[DAY_IN_YEAR] - tm_t.tm_yday < minDay){
-            minDay = friends[j].date[DAY_IN_YEAR] - tm_t.tm_yday;
+    for (uint8_t i = 0; i < friends.size(); ++i){
+        if (TODAY_BIRTHDAY){
+            std::cout << "Today is the birthday \"" << friends[i].name << "\"\n";
+            std::cout << "He will be " << YEARS << " years old\n";
+        }else if (CHECK_1){
+            minDay = MDAY;
         }
     }
 
-    for (int k = 0; k < friends.size(); ++k ) {
-        if ( friends[k].date[DAY_IN_YEAR] == minDay)
-            std::cout << "The nearest birthday of " << friends[k].name << ".\nAnd it is executed "
-                      << tm_t.tm_year - friends[k].date[YEAR] <<" year\n\n";
-
+    for (uint8_t i = 0 ; i < friends.size() ; ++i){
+        if (MDAY == minDay) {
+            std::cout << "Nearest birthday with " << friends[i].name;
+            std::cout << "He will be " << YEARS << " years old\n";
+            break;
+        }
     }
 
     return 0;
