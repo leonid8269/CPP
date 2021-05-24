d62cf8116cd58ffc1099d638577cc2e2592c9355
Leonid <leonid8269@gmail.com>
2021-05-24 17:16:47 +0700

    go check sash)

--- HomeWorkSkillbox/27_time/3/main.cpp
+++ HomeWorkSkillbox/27_time/3/main.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 #include <ctime>
 #include <iomanip>
+#include <string>
+
+
+
+class timer{
+
+private:
+    time_t general = time(nullptr);
+    tm tm_t = *std::localtime(&general);
+
+public:
+    void set_timer(){
+        std::cout << "Enter MM:SS\n";
+        std::cin >> std::get_time(&tm_t, "%M:%S");
+        general += (60 * tm_t.tm_min) + tm_t.tm_sec;
+    }
+
+    void timer_go(){
+        time_t stop{}, buf{};
+        uint32_t save = time(nullptr);
+        bool flag = false;
+        do {
+            stop = time(nullptr);
+            buf = general - stop;
+            std::tm tm_buf = *std::localtime(&buf);
+            if (stop != save) {
+                save = stop;
+                flag = true;
+            }
+            if (flag) {
+                std::cout << "Time left [Min:sec]:  " <<
+                          tm_buf.tm_min << ':' << tm_buf.tm_sec << "  \n";
+                flag = false;
+            }
+        }while(stop < general);
+
+        std::cout << "DING! DING! DING!\n";
+    }
+
+};
 
 
 int main() {
 
-
+    timer one;
+    one.set_timer();
+    one.timer_go();
 
     return 0;
 }
--- HomeWorkSkillbox/TESTint/main.cpp
+++ HomeWorkSkillbox/TESTint/main.cpp
@@ -10,12 +10,7 @@
 int main (){
 
 
-    time_t t = time(nullptr);
-    time_t s = time(nullptr);
 
-    std::tm today = *std::localtime(&s);std::tm t_tm = *std::localtime(&t);
-
-    std::cin >> std::get_time(&t_tm, "%Y/%m/%d");
 
 
 
