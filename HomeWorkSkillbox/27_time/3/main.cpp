7d78a5c369acb1ac4be08b57b7422a1e450cfb8f
Leonid <leonid8269@gmail.com>
2021-05-25 14:09:50 +0700

    fix bag!

--- HomeWorkSkillbox/27_time/3/main.cpp
+++ HomeWorkSkillbox/27_time/3/main.cpp
@@ -6,36 +6,39 @@
 class timer{
 
 private:
-    time_t general = time(nullptr);
+    time_t general{};
     tm tm_t = *std::localtime(&general);
 
 public:
     void set_timer(){
         std::cout << "Enter MM:SS\n";
         std::cin >> std::get_time(&tm_t, "%M:%S");
-        general +=( ( 60 * tm_t.tm_min ) + tm_t.tm_sec );
+
     }
 
-    void timer_go(){
+    void timer_go() {
+
         time_t stop = time(nullptr);
         uint32_t save = time(nullptr);
+
+        general = time(nullptr);
+        general += ( ( 60 * tm_t.tm_min ) + tm_t.tm_sec );
 
         bool flag = false;
         for (  ;stop != general ;stop = time(nullptr) ) {
             time_t buf = general - stop;
             std::tm tm_buf = *std::localtime(&buf);
-            if (stop != save) {
+            if (stop != save){
                 save = stop;
                 flag = true;
             }
 
-            if (flag) {
+            if (flag){
                 std::cout << "Time left [Min:sec]:  " <<
                           tm_buf.tm_min << ':' << tm_buf.tm_sec << "  \n";
                 flag = false;
             }
         }
-
         std::cout << "DING! DING! DING!\n";
     }
 };
