ff944aa3a22e8c8cbef126dbc135b6870ac0063d
Leonid <leonid8269@gmail.com>
2021-05-25 10:50:53 +0700

    i am not know how decide bug(( 5-8 sec elude

--- HomeWorkSkillbox/27_time/3/main.cpp
+++ HomeWorkSkillbox/27_time/3/main.cpp
@@ -3,8 +3,6 @@
 #include <iomanip>
 #include <string>
 
-
-
 class timer{
 
 private:
@@ -15,36 +13,34 @@ public:
     void set_timer(){
         std::cout << "Enter MM:SS\n";
         std::cin >> std::get_time(&tm_t, "%M:%S");
-        general += (60 * tm_t.tm_min) + tm_t.tm_sec;
+        general +=( ( 60 * tm_t.tm_min ) + tm_t.tm_sec );
     }
 
     void timer_go(){
-        time_t stop{}, buf{};
+        time_t stop = time(nullptr);
         uint32_t save = time(nullptr);
+
         bool flag = false;
-        do {
-            stop = time(nullptr);
-            buf = general - stop;
+        for (  ;stop != general ;stop = time(nullptr) ) {
+            time_t buf = general - stop;
             std::tm tm_buf = *std::localtime(&buf);
             if (stop != save) {
                 save = stop;
                 flag = true;
             }
+
             if (flag) {
                 std::cout << "Time left [Min:sec]:  " <<
                           tm_buf.tm_min << ':' << tm_buf.tm_sec << "  \n";
                 flag = false;
             }
-        }while(stop < general);
+        }
 
         std::cout << "DING! DING! DING!\n";
     }
-
 };
 
-
 int main() {
-
     timer one;
     one.set_timer();
     one.timer_go();
